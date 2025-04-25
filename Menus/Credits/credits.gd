de3dd73e24f69155a8623e29d255fc100defc471
Zeta80 <axxel1700@gmail.com>
2025-04-26 01:21:05 +0200

    credits sistemati

---
+++ Menus/Credits/credits.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	await get_tree().create_timer(3.0).timeout
+	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
