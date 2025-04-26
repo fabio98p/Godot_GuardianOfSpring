261a39f52d748e61ef3c679f29abd739d584f7b8
Zeta80 <axxel1700@gmail.com>
2025-04-26 23:34:19 +0200

    immagini prologo sequenze

--- Level/Prologue/prologue.gd
+++ Level/Prologue/prologue.gd
@@ -4,6 +4,7 @@ extends Control
 @onready var panel_black: Panel = $PanelBlack
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @export var texture:Array[String]
+@onready var animation_sfondi: AnimationPlayer = $AnimationSfondi
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,13 +17,29 @@ func _process(delta: float) -> void:
 	pass
 	
 func startPrologue():
-
-	for iteration in texture:
-		change_bg_img(iteration)
-		changeBlackToImage()
-		await get_tree().create_timer(4.0).timeout
-		changeImageToBlack()
-		await get_tree().create_timer(2.0).timeout
+#	inizio sfondo 1
+	changeBlackToImage()
+	animation_sfondi.play("sfondo1")
+	await get_tree().create_timer(4.0).timeout
+	changeImageToBlack()
+	await get_tree().create_timer(2.0).timeout
+	#	inizio sfondo 2
+	changeBlackToImage()
+	animation_sfondi.play("sfondo2")
+	await get_tree().create_timer(4.0).timeout
+	changeImageToBlack()
+	await get_tree().create_timer(2.0).timeout
+	#	inizio sfondo 3
+	changeBlackToImage()
+	animation_sfondi.play("sfondo3")
+	await get_tree().create_timer(4.0).timeout
+	changeImageToBlack()
+	await get_tree().create_timer(2.0).timeout
+	#	inizio sfondo 4
+	changeBlackToImage()
+	animation_sfondi.play("sfondo4")
+	await get_tree().create_timer(4.0).timeout
+	changeImageToBlack()
 	await get_tree().create_timer(2.0).timeout
 	get_tree().change_scene_to_file("res://Level/level.tscn")
 	
